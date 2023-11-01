dad7c2b3d1fd86ab9fb23129420508c714417c90
Jandzsó György <haver1036@gmail.com>
2023-11-01 11:54:52 +0100

    First init - Tiny10, GitBashScripts

---
+++ Tiny10_EditRegistryAndStore.bat
@@ -0,0 +1,10 @@
+::Comment
+
+
+PowerShell -Command "&{Set-SmbClientConfiguration -EnableBandwidthThrottling 0 -EnableLargeMtu 1 -Confirm:$false}"
+PowerShell -Command "&{New-ItemProperty -Path HKLM:\\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters -Name AllowInsecureGuestAuth -Value 1 ` -PropertyType DWORD -Force | Out-Null}"
+REG IMPORT %~dp0Registry\CopyAsPath.reg
+REG IMPORT %~dp0Registry\OpenTerminalHere.reg
+%~dp0MicrosoftStore\Add-Store.cmd
+
+timeout 10
